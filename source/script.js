d7ef78e5423482dbd3eed4e4a626093e9be812b5
mayday31 <brianajlynn@gmail.com>
2025-12-08 16:20:54 -0500

    Added ai API

--- source/script.js
+++ source/script.js
@@ -1,10 +1,21 @@
+function showPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Poem here",
-    autoStart: true,
-  });
+  let enterRequest = document.querySelector("#user-request");
+  let apiKey = "86d11b15bf79a84309ce4be6o80tad43";
+  let prompt = `User instructions: Generate a haiku poem about ${enterRequest.value}`;
+  let context =
+    "You are excellent at making thoughtful, heartfelt haiku poems. Make sure to follow user instructions, and follow traditional haiku poem structure. Use basic HTML, and separate each line with a <br />.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemForm = document.querySelector("#poem-generator-form");
