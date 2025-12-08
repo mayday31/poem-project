e901636ab8067f68a69fb3ab10604720569ec8bc
mayday31 <brianajlynn@gmail.com>
2025-12-08 17:10:00 -0500

    Improved ux

--- source/script.js
+++ source/script.js
@@ -15,6 +15,10 @@ function generatePoem(event) {
     "You are excellent at making thoughtful, heartfelt haiku poems. Make sure to follow user instructions, and follow traditional haiku poem structure. Use basic HTML, and separate each line with a <br />.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poem = document.querySelector("#poem");
+  poem.classList.remove("hide");
+  poem.innerHTML = `Generating a poem about ${enterRequest.value}...`;
+
   axios.get(apiUrl).then(showPoem);
 }
 
